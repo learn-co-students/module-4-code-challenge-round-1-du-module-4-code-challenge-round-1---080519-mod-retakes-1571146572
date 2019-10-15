9be4483ee18134a283a1e6685e39d8b20e808ec4
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-15 09:52:19 -0400

    getting started

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -5,9 +5,14 @@ import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    console.log(this.props)
+    this.state = {
 
+      
+
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
