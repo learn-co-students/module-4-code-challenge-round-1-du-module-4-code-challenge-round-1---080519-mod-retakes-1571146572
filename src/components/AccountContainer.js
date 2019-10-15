3c2a69bef199691898fd92d60df5e37a9830d9e4
CihatAld <cihataldanmaz@gmail.com>
2019-10-15 11:16:00 -0400

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,26 +4,75 @@ import Search from './Search'
 import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
-
   constructor() {
     super()
-
+this.state = {
+  transactions : [],
+  searchValue : ''
+ 
+}
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+        .then(resp => resp.json())
+        .then(data => this.setState({
+          transactions : data
+        }))
+
+  }
+  
+
+  handleChange = (event) => {
+    console.log(event.target.value)
+    event.preventDefault()
+     this.setState({
+      searchValue : event.target.value
+    })
   }
 
-  render() {
 
+
+  displayState = () => {
+
+   return this.state.transactions.filter(transaction => 
+    (transaction.category.toLowerCase()).includes(this.state.searchValue.toLowerCase())
+    )
+    
+  // let catelow = transaction.category.toLowerCase()
+  // let deslow = transaction.description.toLowerCase()
+  // let value = this.state.searchValue.toLowerCase()
+
+  // this.state.transactions.map((transaction) => {
+    
+  // let catelow = transaction.category.toLowerCase()
+  // let deslow = transaction.description.toLowerCase()
+  // let value = this.state.searchValue.toLowerCase()
+
+  //   if (catelow.includes(value)){
+  //     this.setState({
+  //       display : [catelow.includes(value)]
+  //     })
+  //   }
+  //   else{
+  //     this.setState({
+  //       display : [deslow.includes(value)]
+  //     })
+  //   }
+
+  // })
+}
+  
+  render() {
+    console.log(this.categoryOrDesc)
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList transactions = {this.displayState()}/>
       </div>
     )
   }
